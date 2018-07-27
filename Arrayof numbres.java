5f1fb32232d5e90170076d098b541bb55ae35de6
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-27 15:39:14 +0530

    Create Arrayof numbres.java

---
+++ Arrayof numbres.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a value");
+		int a=sc.nextInt();
+		int k=sc.nextInt();
+		int[]a=new int[n];
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextint();
+		}
+		if(k<n)
+		{
+			System.out.println();
+		}
+		else for(int i=0;i<k;i++)
+		{
+			sum+=a[i];
+		}
+		System.out.println(sum);
+		}
+		
+	}
+}

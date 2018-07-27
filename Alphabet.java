7f92298e300069328e765b4967a66aad2621b29e
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-27 14:12:18 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner ss=new Scanner(System.in);
+		Syste.out.println("Enter a char");
+		char ch=sc.next().charAt(0);
+		if(ch='a'&&ch='z'|| ch='A'&&ch='Z')
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			Syatem.out.println("invalid");
+		}
+	}
+}

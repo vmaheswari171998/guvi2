4321ae1a5a659993e3f1be163b8486402d98a1de
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-27 11:15:04 +0530

    Create EvenorOdd.java

---
+++ EvenorOdd.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n=sc.nextInt();
+		if(n<0)
+		{
+			System.out.println("Invalid");
+		}
+		else if(n/2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+}

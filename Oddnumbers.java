823afc13075ec86762780e95a5ed1b277474abf6
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-31 12:23:38 +0530

    Create Oddnumbers.java

---
+++ Oddnumbers.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter range of numbers");
+		range(10,20);
+		if(range/2 !=0)
+		{
+			System.out.println("Odd number");
+		}
+		else
+		{
+			System.out.println("not odd number");
+		}
+	}
+}

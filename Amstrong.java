74d4444244a487c4a3a7f0d276e0d5b03c159a82
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-31 14:47:43 +0530

    Create Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+{	
+ int num = 370, number, temp, total = 0;
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+}
+
+		
+

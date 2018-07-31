f39e024f05ded467b9d4adddd69e94e2d364fc90
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-31 12:03:02 +0530

    Create Palindromnumber.java

---
+++ Palindromnumber.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		class PalindromeExample
+		{  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+} 
+	

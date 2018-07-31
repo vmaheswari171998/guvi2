19826a8200707ee85f13764d8cf9bba4c0c8d08f
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-07-31 12:15:11 +0530

    Create Powerofnumber.java

---
+++ Powerofnumber.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.lang.Math;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a=sc.nextInt();
+		System.out.println("Enter b value");
+		int b=sc.nextInt();
+		System.out.println(Math.pow(a,b));
+	}
+}

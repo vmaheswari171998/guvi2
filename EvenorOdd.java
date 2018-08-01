0df71c01cda3f4eb031fefe14b9b199dee659503
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-01 14:19:11 +0530

    Update EvenorOdd.java

--- EvenorOdd.java
+++ EvenorOdd.java
@@ -5,18 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class EvenorOdd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number");
 		int n=sc.nextInt();
 		if(n<0)
 		{
 			System.out.println("Invalid");
 		}
-		else if(n/2==0)
+		else if(n%2==0)
 		{
 			System.out.println("Even");
 		}

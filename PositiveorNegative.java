d88dcd46a53436d4dc73fc39137e397f5ef248c8
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-01 14:15:44 +0530

    Update PositiveorNegative.java

--- PositiveorNegative.java
+++ PositiveorNegative.java
@@ -5,20 +5,19 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class PositiveorNegative
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a value");
 		int a=sc.nextInt();
-		if(a=0)
+		if(a==0)
 		{
 			System.out.println("Zero");
 		}
 		else if(a>0)
 		{
-			System.out.printl("Positive");
+			System.out.println("Positive");
 		}
 		else
 		{

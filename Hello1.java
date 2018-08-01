82423fc0f0ddd75eefa748a96408a0b1f4121505
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-01 14:49:49 +0530

    Update and rename Hello.java to Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -5,19 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Hello1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number");
-		int a=sc.nextInt();
-		int count=0;
-		if(a !=0)
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
 		{
-			a=a/10;
-			++count;
+			System.out.println("Hello");
 		}
-		System.out.println("count");
 	}
 }

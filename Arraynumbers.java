0402c26087aae15a6845e82be80b6cccbd41f729
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-01 15:26:39 +0530

    Update Arraynumbers.java

--- Arraynumbers.java
+++ Arraynumbers.java
@@ -10,20 +10,20 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a value");
-		int a=sc.nextInt();
+		int n=sc.nextInt();
 		int k=sc.nextInt();
-		int[]a=new int[n];
+		int[] a=new int[n];
 		int sum=0;
 		for(int i=0;i<n;i++)
 		{
-			a[i]=sc.nextint();
+			a[i]=sc.nextInt();
 		}
-		if(k<n)
+		if(k>=n)
 		{
 			System.out.println();
 		}
-		else for(int i=0;i<k;i++)
+		else 
+			for(int i=0;i<k;i++)
 		{
 			sum+=a[i];
 		}

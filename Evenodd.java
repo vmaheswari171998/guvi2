f2fb3bf90c0f9cdf5bf58138390e7aa23b66348f
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-08 19:42:19 +0530

    Create Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int res=n-m;
+		res=Math.abs(res);
+		if(res%2==0)
+			System.out.println("even");
+		else
+			System.out.println("no");
+	}
+}

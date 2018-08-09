483abf00dc44be14d83b28ac9b1e722f4ef48e20
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-09 12:12:12 +0530

    Update Primenumber.java

--- Primenumber.java
+++ Primenumber.java
@@ -6,7 +6,6 @@ public class Prime {
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
-            // condition for nonprime number
             if(num % i == 0)
             {
                 flag = true;
@@ -15,9 +14,9 @@ public class Prime {
         }
 
         if (!flag)
-            System.out.println(num + " is a prime number.");
+            System.out.println("yes");
         else
-            System.out.println(num + " is not a prime number.");
+            System.out.println("no");
     }
 }
-When you run
+

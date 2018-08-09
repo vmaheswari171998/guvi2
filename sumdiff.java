6f7458278250293683081cf54ee69de7ad5ebc06
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-09 12:23:23 +0530

    Create sumdiff.java

---
+++ sumdiff.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=0;
+		sum=a+b;
+		System.out.println(sum);
+	}
+}

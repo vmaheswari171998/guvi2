703a225ecdddbf405086bc7ee27ff184a235efea
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-12 12:11:46 +0530

    Rename naturalnumberssum.java to Naturalnumberssum.java

---
+++ Naturalnumberssum.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int total=0,count=1;
+		 while(count <= num)
+       {
+           total = total + count;
+           count++;
+       }
+       System.out.println(total);
+		
+	}
+}

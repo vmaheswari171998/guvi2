5c7e0e1ad89bf0aeb841ce1634dde880b36e35af
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-13 09:54:44 +0530

    Create stringnumber.java

---
+++ stringnumber.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		if(ch>='0' && ch<='9')
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}

2ba12594f9d584424ef59db3818a7ad6ba0ece4c
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-13 11:17:16 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("\n"+a[i]+" "+i);
+		}
+	}
+}

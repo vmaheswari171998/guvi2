b4272412a8756b598137d3d0456be091881d6551
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-13 12:31:26 +0530

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int f=a*b;
+		int g=f/c;
+		System.out.println(g);
+	}
+}

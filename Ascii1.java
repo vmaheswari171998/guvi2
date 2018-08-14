7dcf12814ed82c5a11f416096d544805251027bb
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-14 11:42:49 +0530

    Update and rename Ascii.java to Ascii1.java

--- Ascii1.java
+++ Ascii1.java
@@ -10,8 +10,8 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		char ch=sc.next().charAt(0);
-		int a=ch;
-		System.out.println(a);
+		char c=sc.next().charAt(0);
+		int b=c;
+		System.out.println(b);
 	}
 }

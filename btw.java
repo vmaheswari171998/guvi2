f400fd4c7437c8a466a04401200ca1658f5671ab
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-08-23 10:35:34 +0530

    Create btw.java

---
+++ btw.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b && b<c)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}

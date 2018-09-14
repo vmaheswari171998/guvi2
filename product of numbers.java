0eee60b97535ee4c713e91f2a86060118801233e
vmaheswari171998 <41770570+vmaheswari171998@users.noreply.github.com>
2018-09-14 12:32:45 +0530

    Create product of numbers.java

---
+++ product of numbers.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		int e=sc.nextInt();
+		int s=a*b*c*d*e;
+		System.out.println(s);
+	}
+}
